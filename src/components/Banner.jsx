7cf190527c4b37c35c19b30a8bc4251582503bb8
nandinigit33 <nandinids33@gmail.com>
2024-08-25 03:04:29 -0400

    committing orbit report assignment

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styling.css';
+
+function Banner () {
+    return (
+    <header className='header'>
+        <h1>Orbit Report</h1>
+        <p className='p'>
+            Click on the buttons to see the satellites in that orbit type.
+        </p>
+    </header>
+    );
+};
+
+ export default Banner;
